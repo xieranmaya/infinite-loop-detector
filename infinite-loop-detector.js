fbeba79d95a07dc041b73620d0ab6245b52f4232
xieranmaya <xieranmaya@gmail.com>
2016-11-25 20:40:20 +0800

    add comment and detect wrap's arg

--- infinite-loop-detector.js
+++ infinite-loop-detector.js
@@ -1,25 +1,31 @@
 var infiniteLoopDetector = (function() {
   var map = {}
 
+  // define an InfiniteLoopError class
+  function InfiniteLoopError(msg, type) {
+    Error.call(this ,msg)
+    this.type = 'InfiniteLoopError'
+  }
+  
   function infiniteLoopDetector(id) {
-    if (id in map) { // 非首次执行
+    if (id in map) { // 非首次执行，此处可以优化，性能太低
       if (Date.now() - map[id] > 1000) {
         delete map[id]
-        throw {
-          type: 'InfiniteLoopError',
-          message: 'Loop runing too long!',
-        }
-        // throw new Error('Loop runing too long!')
+        throw new Error('Loop runing too long!', 'InfiniteLoopError')
       }
     } else { // 首次运行，记录循环开始的时间。之所有把非首次运行的判断写在前面的if里是因为上面会执行更多次
       map[id] = Date.now()
     }
   }
 
-  infiniteLoopDetector.wrap = function(code) {
-    return code.replace(/for *\(.*\{|while *\(.*\{|do *\{/g, function(line) {
+  infiniteLoopDetector.wrap = function(codeStr) {
+    if (typeof codeStr !== 'string') {
+      throw new Error('Can only wrap code represented by string, not any other thing at the time! If you want to wrap a function, convert it to string first.')
+    }
+    // this is not a strong regex, but enough to use at the time
+    return codeStr.replace(/for *\(.*\{|while *\(.*\{|do *\{/g, function(loopHead) {
       var id = parseInt(Math.random() * Number.MAX_SAFE_INTEGER)
-      return `infiniteLoopDetector(${id});${line}infiniteLoopDetector(${id});`
+      return `infiniteLoopDetector(${id});${loopHead}infiniteLoopDetector(${id});`
     })
   }
 
